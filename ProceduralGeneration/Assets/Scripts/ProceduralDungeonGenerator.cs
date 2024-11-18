0a314e66f4423c8eb5e06e0e6a363147eda88555
Fernando Restituto <linksgreenhat@gmail.com>
2024-11-17 22:35:09 -0500

    sample code iteration

--- ProceduralGeneration/Assets/Scripts/ProceduralDungeonGenerator.cs
+++ ProceduralGeneration/Assets/Scripts/ProceduralDungeonGenerator.cs
@@ -11,43 +11,33 @@ public static partial class ProceduralDungeonGenerator
                 UnityEngine.Debug.Log("Roll with 10% chance: False");
         }
 
-        Room startingRoom = AddRoom(RoomType.Start, new Coordinate(0, 0));
+        Room startRoom0x0 = AddRoom(RoomType.Start, new Coordinate(0, 0));
+        Room room1x0 = AddRoom(RoomType.Normal, new Coordinate(1, 0));
+        Room room_1x0 = AddRoom(RoomType.Normal, new Coordinate(-1, 0));
+        Room room0x_1 = AddRoom(RoomType.Normal, new Coordinate(0, -1));
+        Room shopRoom_2x0 = AddRoom(RoomType.Shop, new Coordinate(-2, 0));
+        Room room1x_1 = AddRoom(RoomType.Trap, new Coordinate(1, -1));
+        Room room0x_2 = AddRoom(RoomType.Normal, new Coordinate(0, -2));
+        Room room0x_3 = AddRoom(RoomType.Trap, new Coordinate(0, -3));
+        Room treasureRoom1x_3 = AddRoom(RoomType.Treasure, new Coordinate(1, -3));
+        Room secretRoom1x_2 = AddRoom(RoomType.Secret, new Coordinate(1, -2));
+        Room bossRoom_1x_3 = AddRoom(RoomType.Boss, new Coordinate(-1, -3));
+        Room superSecretRoom0x_4 = AddRoom(RoomType.SuperSecret, new Coordinate(0, -4));
 
-        Room roomWestOfStartingRoom = AddRoom(RoomType.Normal, new Coordinate(1, 0));
-        AddDoor(DoorType.Open, startingRoom, roomWestOfStartingRoom);
-
-        Room roomEastOfStartingRoom = AddRoom(RoomType.Normal, new Coordinate(-1, 0));
-        AddDoor(DoorType.Open, startingRoom, roomEastOfStartingRoom);
-
-        Room roomSouthOfStartingRoom = AddRoom(RoomType.Normal, new Coordinate(0, -1));
-        AddDoor(DoorType.Open, startingRoom, roomSouthOfStartingRoom);
-
-        Room shopRoom = AddRoom(RoomType.Shop, new Coordinate(-2, 0));
-        AddDoor(DoorType.Locked, roomEastOfStartingRoom, shopRoom);
-
-        Room roomWithPassageToSecretRoom = AddRoom(RoomType.Trap, new Coordinate(1, -1));
-        AddDoor(DoorType.Open, roomWestOfStartingRoom, roomWithPassageToSecretRoom);
-        AddDoor(DoorType.Open, roomSouthOfStartingRoom, roomWithPassageToSecretRoom);
-
-        Room corridorRoom1 = AddRoom(RoomType.Normal, new Coordinate(0, -2));
-        AddDoor(DoorType.Open, roomSouthOfStartingRoom, corridorRoom1);
-
-        Room corridorRoom2 = AddRoom(RoomType.Trap, new Coordinate(0, -3));
-        AddDoor(DoorType.Open, corridorRoom1, corridorRoom2);
-
-        Room treasureRoom = AddRoom(RoomType.Treasure, new Coordinate(1, -3));
-        AddDoor(DoorType.Locked, corridorRoom2, treasureRoom);
-
-        Room secretRoom = AddRoom(RoomType.Secret, new Coordinate(1, -2));
-        AddDoor(DoorType.Bombable, roomWithPassageToSecretRoom, secretRoom);
-        AddDoor(DoorType.Bombable, corridorRoom1, secretRoom);
-        AddDoor(DoorType.Bombable, treasureRoom, secretRoom);
-
-        Room bossRoom = AddRoom(RoomType.Boss, new Coordinate(-1, -3));
-        AddDoor(DoorType.Open, corridorRoom2, bossRoom);
-
-        Room superSecretRoom = AddRoom(RoomType.SuperSecret, new Coordinate(0, -4));
-        AddDoor(DoorType.Bombable, corridorRoom2, superSecretRoom);
+        AddDoor(DoorType.Open, startRoom0x0, room1x0);
+        AddDoor(DoorType.Open, startRoom0x0, room_1x0);
+        AddDoor(DoorType.Open, startRoom0x0, room0x_1);
+        AddDoor(DoorType.Locked, room_1x0, shopRoom_2x0);
+        AddDoor(DoorType.Open, room1x0, room1x_1);
+        AddDoor(DoorType.Open, room0x_1, room1x_1);
+        AddDoor(DoorType.Open, room0x_1, room0x_2);
+        AddDoor(DoorType.Open, room0x_2, room0x_3);
+        AddDoor(DoorType.Locked, room0x_3, treasureRoom1x_3);
+        AddDoor(DoorType.Bombable, room0x_2, secretRoom1x_2);
+        AddDoor(DoorType.Bombable, room1x_1, secretRoom1x_2);
+        AddDoor(DoorType.Bombable, treasureRoom1x_3, secretRoom1x_2);
+        AddDoor(DoorType.Open, room0x_3, bossRoom_1x_3);
+        AddDoor(DoorType.Bombable, room0x_3, superSecretRoom0x_4);
         
         UnityEngine.Debug.Log("There are " + GetDungeonRooms().Count + " rooms in this dungeon.");
         UnityEngine.Debug.Log("There are " + GetDungeonDoors().Count + " doors in this dungeon.");
