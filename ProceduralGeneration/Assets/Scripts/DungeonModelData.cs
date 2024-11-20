97cfea5e482177828a729243c553c60efefe711a
Jakob Wax <waxjakob@gmail.com>
2024-11-20 14:24:32 -0500

    My WIP

--- ProceduralGeneration/Assets/Scripts/DungeonModelData.cs
+++ ProceduralGeneration/Assets/Scripts/DungeonModelData.cs
@@ -7,6 +7,8 @@ public static partial class ProceduralDungeonGenerator
     static LinkedList<Door> doors;
     static System.Random rand;
     const int SeedForRandomGeneration = 1337;
+    private const int MaximumRoomDepth = 5;
+    private const float FloorPercentChanceModifier = 1.0f;
 
     public static void Init()
     {
@@ -60,8 +62,55 @@ public static partial class ProceduralDungeonGenerator
     {
         return doors;
     }
-    
 
+    public static bool CheckIfCoordinateIsEmpty(Coordinate coordinate)
+    {
+        foreach (var room in rooms)
+        {
+            if (room.coordinate.IsEqualTo(coordinate))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool CheckIfCoordinateAndNeighboursIsEmpty(Coordinate coordinate)
+    {
+        foreach (var room in rooms)
+        {
+            if (room.coordinate.IsEqualTo(coordinate))
+            {
+                return false;
+            }
+        }
+
+        Coordinate xPlusOne = new Coordinate(coordinate.x + 1, coordinate.y);
+        Coordinate yPlusOne = new Coordinate(coordinate.x, coordinate.y + 1);
+        Coordinate xMinusOne = new Coordinate(coordinate.x - 1, coordinate.y);
+        Coordinate yMinusOne = new Coordinate(coordinate.x, coordinate.y - 1);
+
+        bool isxPlusOneFree = CheckIfCoordinateIsEmpty(xPlusOne);
+        bool isyPlusOneFree = CheckIfCoordinateIsEmpty(yPlusOne);
+        bool isxMinusOneFree = CheckIfCoordinateIsEmpty(xMinusOne);
+        bool isyMinusOneFree = CheckIfCoordinateIsEmpty(yMinusOne);
+
+        return (isxPlusOneFree && isxMinusOneFree) || (isyPlusOneFree && isyMinusOneFree);
+    }
+
+    public static Room FindRoomOfType(RoomType type)
+    {
+        foreach (var room in rooms)
+        {
+            if (room.type == type)
+            {
+                return room;
+            }
+        }
+
+        return null;
+    }
 }
 
 public enum RoomType
@@ -114,6 +163,8 @@ public class Room : DungeonModelData
     public RoomType type;
     public Coordinate coordinate;
     public LinkedList<Door> doors;
+
+    public int depth = 0;
 
     public Room(RoomType type, Coordinate coordinate)
     {
